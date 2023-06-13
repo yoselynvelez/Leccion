ca252dee86a1c775c044edb5984699985ccbbead
Yoselyn Velez <yoselyn.velezc@ug.edu.ec>
2023-06-12 22:26:38 -0500

    Leccion Aplicaciones Moviles

---
+++ leccionYose/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'leccionYose',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
